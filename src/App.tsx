44635358fa3e8ae7c01c2ed2856bdf07309ea950
ArtemZvyagin <109950631+ArtemZvyagin@users.noreply.github.com>
2023-06-13 22:25:59 +0300

    Login by rainbow; add mui; etc...

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import {wagmiConfig} from "./configs/wagmi";
+import { WagmiConfig, useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+function Profile() {
+  debugger
+  const { address, isConnected } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
+
+  if (isConnected)
+    return (
+      <div>
+        Connected to {address}
+        <Button color={'inherit'} onClick={() => disconnect()}>Disconnect</Button>
+      </div>
+    )
+
+  return <Button color={'inherit'} onClick={() => connect()}>Connect Wallet</Button>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig config={wagmiConfig}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Yield Granter
+            </Typography>
+
+            <Profile />
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </WagmiConfig>
   );
 }
 
